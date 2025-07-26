33f9db058bab4266093e57a22e0672991ef6466f
Sakuni <saku9706@gmail.com>
2025-07-26 13:45:29 +0530

    initial commit

---
+++ src/main/java/com/smartdelivery/SmartdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.smartdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdeliveryApplication.class, args);
+	}
+
+}
